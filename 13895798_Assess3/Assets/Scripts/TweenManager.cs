c52ab7b57dcb6412bdf1c0de2156605abf54da04
LukaRyan01 <lukaryan01@gmail.com>
2022-09-22 12:34:55 +1000

    Added initial script for tween movement functionality

---
+++ 13895798_Assess3/Assets/Scripts/TweenManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private Tweener tweener;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
