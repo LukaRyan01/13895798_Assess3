a9d7eac9699893585240f970380ac8948bce8842
LukaRyan01 <lukaryan01@gmail.com>
2022-10-26 21:47:35 +1100

    Added animation to match movement, needs adjustment

--- 13895798_Assess3/Assets/Scripts/PacStudentController.cs
+++ 13895798_Assess3/Assets/Scripts/PacStudentController.cs
@@ -11,6 +11,7 @@ public class PacStudentController : MonoBehaviour
     private Tweener tweener;
     private Vector3 KirbyNewPos;
     public Tilemap TileMap;
+    public Animator AnimationController;
 
     // Start is called before the first frame update
     void Start()
@@ -39,15 +40,18 @@ public class PacStudentController : MonoBehaviour
         }
         if (!(tweener.TweenExists(kirby.transform)))
         {
+            
             if (lastInput == KeyCode.W)
             {
                 KirbyNewPos = kirby.transform.position + new Vector3(0.0f, 1.0f, 0.0f);
                 if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))) == null)
                 {
+                    AnimationController.SetTrigger("UpParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, KirbyNewPos, 1.0f);
                 }
                 else if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))).name == "Normal Pellet Simple Tile")
                 {
+                    AnimationController.SetTrigger("UpParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, KirbyNewPos, 1.0f);
                 }
             }
@@ -56,10 +60,12 @@ public class PacStudentController : MonoBehaviour
                 KirbyNewPos = kirby.transform.position + new Vector3(-1.0f, 0.0f, 0.0f);
                 if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))) == null)
                 {
+                    AnimationController.SetTrigger("LeftParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, KirbyNewPos, 1.0f);
                 }
                 else if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))).name == "Normal Pellet Simple Tile" )
                 {
+                    AnimationController.SetTrigger("LeftParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, (kirby.transform.position + new Vector3(-1.0f, 0.0f, 0.0f)), 1.0f);
                }
             }
@@ -69,10 +75,12 @@ public class PacStudentController : MonoBehaviour
                 KirbyNewPos = kirby.transform.position + new Vector3(0.0f, -1.0f, 0.0f);
                 if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))) == null)
                 {
+                    AnimationController.SetTrigger("DownParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, KirbyNewPos, 1.0f);
                 }
                 else if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))).name == "Normal Pellet Simple Tile")
                 {
+                    AnimationController.SetTrigger("DownParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, (kirby.transform.position + new Vector3(0.0f, -1.0f, 0.0f)), 1.0f);
                 }
             }
@@ -81,10 +89,12 @@ public class PacStudentController : MonoBehaviour
                 KirbyNewPos = kirby.transform.position + new Vector3(1.0f, 0.0f, 0.0f);
                 if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))) == null)
                 {
+                    AnimationController.SetTrigger("RightParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, KirbyNewPos, 1.0f);
                 }
                 else if (TileMap.GetTile(Vector3Int.FloorToInt(KirbyNewPos + new Vector3(-4.5f, -1.5f, 0.0f))).name == "Normal Pellet Simple Tile")
                 {
+                    AnimationController.SetTrigger("RightParam");
                     tweener.AddTween(kirby.transform, kirby.transform.position, (kirby.transform.position + new Vector3(1.0f, 0.0f, 0.0f)), 1.0f);
                 }
             }
